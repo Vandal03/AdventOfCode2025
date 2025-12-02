6397bc114318af344bbcddb1c0681df9cfb5f6ce
Abraham Vanderpool <abevanderpool228@gmail.com>
2025-12-02 18:09:51 -0500

    Add files via upload

---
+++ Advent of Code/Day 1/puzzleOne.py
@@ -0,0 +1,41 @@
+class combination:
+    def __init__(self):
+        self.zero_count = 0
+        self.starting_pos = 50
+
+    def update_dial(self, turn):
+        self.turnLen = len(turn)
+        self.direction = turn[0]
+        self.clicks = int(turn[1:(self.turnLen-1)]) % 100
+        if self.direction == "L":
+            self.click = 0
+            while self.click < self.clicks:
+                if self.starting_pos == 99:
+                    self.starting_pos = 0
+                    self.click += 1
+                else:
+                    self.starting_pos = self.starting_pos + 1
+                    self.click += 1
+        else:
+            self.click = 0
+            while self.click < self.clicks:
+                if self.starting_pos == 0:
+                    self.starting_pos = 99
+                    self.click += 1
+                else:
+                    self.starting_pos = self.starting_pos - 1
+                    self.click +=1
+        if self.starting_pos == 0:
+            self.zero_count += 1
+
+if __name__ == "__main__":
+    
+    comb = combination()
+    
+    with open("turns.txt") as content:
+        turns = content.readlines()
+        for turn in turns:
+            comb.update_dial(turn)
+
+    print(comb.zero_count)
+                
---
+++ Advent of Code/Day 2/puzzleTwo.py
@@ -0,0 +1,44 @@
+import csv
+
+
+class invalid_id_checker():
+    def __init__(self):
+        with open("Day 2/ranges.txt") as content:
+            line = content.readline()
+            self.ids = line.split(",")
+            self.toCount = []
+        
+    def run_check(self):
+        for id in self.ids:
+            minMax = id.split("-")
+            min = minMax[0]
+            max = minMax[1]
+            i = int(minMax[0])
+            while i <= int(max):
+                length = len(str(i))
+                if length % 2 == 0:
+                    half = int(length/2)
+                    halfOne = str(i)[0:half]
+                    halfTwo = str(i)[half:length]
+                    if halfOne == halfTwo:
+                        self.toCount.append(i)
+                        print(i)
+                    i += 1
+                else:
+                    i+=1
+
+        finalCount = 0
+        for id in self.toCount:
+            finalCount += id
+        print(f"Final Count: {finalCount}")
+            
+
+
+
+if __name__ == "__main__":
+    checker = invalid_id_checker()
+    checker.run_check()
+
+
+
+
