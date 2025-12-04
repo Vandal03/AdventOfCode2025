14bd58bcf38e93cc0ce0294ac59a8ff0879841e3
Abraham Vanderpool <abraham@Abrahams-MacBook-Air.local>
2025-12-03 22:20:39 -0500

    2nd go - no luck

--- Day3/puzzleTwo.py
+++ Day3/puzzleTwo.py
@@ -8,52 +8,36 @@ class JoltageFinder():
 
 
     def find_joltage(self):
-        
         for battery in self.batteryBanks:
             self.joltageArray = []
-            self.batteryRemaining = len(battery) - 1 
-            for num in battery[:-1]:
-                match len(self.joltageArray):
-                    case 0:
-                       self.joltageArray.append(num)
-                    case 1|2|3|4|5|6|7|8|9|10|11|12:
-                       self.check_array(num)
+            self.batteryLengthRemaining = len(battery) - 1
+            while self.batteryLengthRemaining > 12:
+                for num in battery[:-1]:
+                    index = 0
+                    self.batteryLengthRemaining -= 1
+                    if len(self.joltageArray) == 0:
+                        self.joltageArray.append(num)
+                    else:
+                        for value in self.joltageArray:
+                            
+                            if int(num) > int(value):
+                                self.joltageArray[index] = num
+                                self.joltageArray = self.joltageArray[0:index + 1]
+                                break
+                            index += 1
+            ## if 12 > index 0
+            ## if 11 > index 1
+            index = -13
+            for value in self.joltageArray:
+                if int(value) < int(battery[index]):
+                    
 
-                self.batteryRemaining -= 1
 
-        print(self.joltageArray)
-
-
-
-    def check_array(self, num):
-        index = 0
-        joltageArrayLength = len(self.joltageArray)
-        for value in self.joltageArray:
-            #Work on first if statement
-            if joltageArrayLength % self.batteryRemaining == 0:
-                self.joltageArray.append(num)
-                break
-            elif int(num) > int(value):
-                self.joltageArray[index] = num
-                self.joltageArray = self.joltageArray[0:index + 1]
-                break
-            elif (index + 1) == joltageArrayLength:
-                self.joltageArray.append(num)
-                
             
-            index += 1
             
-                
-
-
-
-
-
-
-
-
-
-
+            
+            
+            
 
 
 if __name__ == "__main__":
